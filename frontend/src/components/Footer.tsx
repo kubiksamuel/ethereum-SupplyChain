0372c50baa287b5efdbdabff0ef129553ffc641d
Samuel Kubik <kubik.samuel@gmail.com>
2022-05-17 10:00:05 +0200

    Beautify footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,6 +4,6 @@ import React from "react";
 export const Footer = () => (
     <footer className="footer">
         <div>&copy; Samuel Kubík</div> 
-        <div><a href="https://www.flaticon.com/free-icons/product" title="product icons">Product icons created by Freepik - Flaticon</a></div>
+        <div><a id="flaticonLink" href="https://www.flaticon.com/free-icons/product" target="_blank" title="Product icons">Product icons created by Freepik - Flaticon</a></div>
    </footer>
   );
